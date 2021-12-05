ae26605a07e5a7b47240c612f640c59120ca7ac0
Thibaut Patel <thibaut@ghost.org>
2021-12-05 22:10:35 +0100

    Added a template with comments
    - Explains each line of my default input parsing logic
    - Takes into account a Windows issue that was reported in the YouTube comments

---
+++ template.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const lines = fs
+  .readFileSync("day??.txt", { encoding: "utf-8" }) // read day??.txt content
+  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .split("\n") // Split on newline
+  .filter(Boolean) // Remove empty lines
+  .map(Number); // Parse each line into a number
