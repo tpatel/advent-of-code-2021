3c4458097638a2c4dacd49e3f76727f06f0f7c46
Thibaut Patel <thibaut@ghost.org>
2021-12-20 22:38:44 +0100

    Updated template to make it easier to understand
    Instead of filtering empty lines (often due to a line return at the end of a file), we remove all starting/ending whitespace with the .trim() function

--- template.js
+++ template.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const lines = fs
   .readFileSync("day??.txt", { encoding: "utf-8" }) // read day??.txt content
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+  .trim() // Remove starting/ending whitespace
   .split("\n") // Split on newline
-  .filter(Boolean) // Remove empty lines
   .map(Number); // Parse each line into a number
 
 function part1() {
